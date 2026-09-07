74f9fcea97fff65116dfbd07d794c633f0307af2
lakpao5 <lakpasherpa9865@gmail.com>
2026-09-07 14:44:11 +0545

    Fix frontend deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL || "https://sathiko-alumni.onrender.com"}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL || "https://sathiko-alumni-nvri.onrender.com"}/api`,
   headers: {
     "Content-Type": "application/json"
   }
